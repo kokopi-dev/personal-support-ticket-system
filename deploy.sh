f3df537a815ead3295c0aacb94836d9eae165c3c
kokopi <haruspace.dev@gmail.com>
2026-03-10 01:15:48 +0900

    fix:deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "Pulling latest code..."
 git pull
 
 echo "Building and restarting containers..."
-docker compose --env-file ./frontend/.env up -d --build
+docker compose up -d --build
 
 echo "Cleaning up old images..."
 docker image prune -f
